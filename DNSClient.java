fc0cc6b3ea6a842e4bf98241f3a334f41377ca64
NetNinja1729 <64086059+NetNinja1729@users.noreply.github.com>
2022-07-13 16:31:52 +0530

    Add files via upload

---
+++ DNSClient.java
@@ -0,0 +1,27 @@
+// import java.rmi.*;
+// import java.rmi.registry.*;
+
+// public class DNSClient {
+//     public static void main(String args[]) throws Exception {
+//         Registry r = LocateRegistry.getRegistry("localhost", 1234);
+//         DNSServerIntf d = (DNSServerIntf) r.lookup("mvsrserver");
+//         String str = args[0];
+//         System.out.println("The website name is:" + str);
+//         System.out.println("The IP Address is:" + d.DNS(str));
+//     }
+// }
+
+
+
+import java.rmi.*;
+import java.rmi.registry.*;
+
+public class DNSClient {
+    public static void main(String args[]) throws Exception{
+        Registry r = LocateRegistry.getRegistry("localhost",1234);
+        DNSServerIntf d = (DNSServerIntf) r.lookup("mvsrserver");
+        String str = args[0];
+        System.out.println("The website is "+ str);
+        System.out.println("The ip address is "+ d.DNS(str));
+    }
+}
---
+++ DNSServerIntf.java
@@ -0,0 +1,9 @@
+// import java.rmi.*;
+// public interface DNSServerIntf extends Remote {
+// public String DNS(String s1) throws RemoteException; }
+
+
+import java.rmi.*;
+public interface DNSServerIntf extends Remote{
+    public String DNS(String s1) throws RemoteException;
+}
